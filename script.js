ac4767dbd807860234aec786ccd8a96d8ed54a7f
farhan <bazariohelp@gmail.com>
2025-03-14 21:36:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showProposal() {
+    document.getElementById("proposal").classList.remove("hidden");
+}
+
+function acceptProposal() {
+    alert("Yay! You said YES! 😍💍❤️ I love you!");
+}
+
+function rejectProposal() {
+    alert("Oh no! 😢 But I will always love you. ❤️");
+}
